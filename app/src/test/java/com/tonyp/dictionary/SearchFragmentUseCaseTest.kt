178244340eb6547351b0036ce2fd53e38af1b67b
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2024-03-13 02:33:11 +0300

    fixed unit test of use case

--- app/src/test/java/com/tonyp/dictionary/SearchFragmentUseCaseTest.kt
+++ app/src/test/java/com/tonyp/dictionary/SearchFragmentUseCaseTest.kt
@@ -7,9 +7,11 @@ import com.tonyp.dictionary.fragment.search.SearchFragmentUseCase
 import com.tonyp.dictionary.service.DictionaryService
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -20,6 +22,7 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import retrofit2.Response
+import java.io.IOException
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class SearchFragmentUseCaseTest {
@@ -40,14 +43,9 @@ class SearchFragmentUseCaseTest {
 
     private val dictionaryService: DictionaryService = mockk()
     private val testDispatcher = StandardTestDispatcher()
+    private val callProcessor: NetworkCallProcessor = mockk()
 
-    private val useCase = SearchFragmentUseCase(
-        NetworkCallProcessor(
-            securePreferences = mockk(),
-            dictionaryService = dictionaryService,
-            authService = mockk()
-        )
-    )
+    private val useCase = SearchFragmentUseCase(callProcessor)
 
     @Before
     fun setUp() = Dispatchers.setMain(testDispatcher)
@@ -59,6 +57,18 @@ class SearchFragmentUseCaseTest {
             coEvery {
                 dictionaryService.search(getSearchRequestMatcher(word))
             } returns Response.success(response)
+            coEvery {
+                callProcessor.dictionaryService(
+                    any(),
+                    captureCoroutine<suspend DictionaryService.() -> Response<MeaningSearchResponse>>()
+                )
+            } answers {
+                Result.success(
+                    runBlocking {
+                        secondArg<suspend DictionaryService.() -> Response<MeaningSearchResponse>>()(dictionaryService).body()!!
+                    }
+                )
+            }
             val searchResult = useCase.search(word)
             coVerify(exactly = 1) { dictionaryService.search(getSearchRequestMatcher(word)) }
             assertEquals(Result.success(response), searchResult)
@@ -71,6 +81,16 @@ class SearchFragmentUseCaseTest {
             coEvery {
                 dictionaryService.search(getSearchRequestMatcher(word))
             } returns Response.error(400, "error".toResponseBody())
+            coEvery {
+                callProcessor.dictionaryService(
+                    any(),
+                    captureCoroutine<suspend DictionaryService.() -> Response<MeaningSearchResponse>>())
+            } answers {
+                runBlocking {
+                    secondArg<suspend DictionaryService.() -> Response<MeaningSearchResponse>>()(dictionaryService)
+                }
+                Result.failure(IOException())
+            }
             val searchResult = useCase.search(word)
             coVerify(exactly = 1) { dictionaryService.search(getSearchRequestMatcher(word)) }
             assertTrue(searchResult.isFailure)
