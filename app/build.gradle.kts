426a71334c270c8d88adbaa1e6e875d8c6e10f5e
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2024-03-15 11:15:59 +0300

    added correct status bar appearance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.tonyp.dictionary"
-        minSdk = 28
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
